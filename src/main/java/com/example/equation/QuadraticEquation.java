d7cfae0a73726082ee2049d3f081ae0ec2bc39c6
Белоушкин Александр Николаевич <aleks.beloushkin@yahoo.com>
2022-12-11 20:28:49 +0300

    Add test case for A = 0, make implementation

--- src/main/java/com/example/equation/QuadraticEquation.java
+++ src/main/java/com/example/equation/QuadraticEquation.java
@@ -3,16 +3,31 @@ package com.example.equation;
 
 public class QuadraticEquation {
 
+    private static final int PRECISION = -10; // epsilon power, means 10^PRECISION
+
     public double[] solve(double a, double b, double c) {
+
+        if (equalDoubles(a, 0)) {
+            throw new IllegalArgumentException("A must not be equal zero");
+        }
+
         double[] result = null;
         double discriminant = b * b - 4 * a * c;
+
         if (discriminant < 0) {
             result = new double[]{};
         } else if (discriminant > 0) {
             double solution1 = (-b - Math.sqrt(discriminant)) / (2 * a);
             double solution2 = (-b + Math.sqrt(discriminant)) / (2 * a);
             result = new double[]{solution1, solution2};
+        } else {
+            double solution = -b / (2 * a);
+            result = new double[]{solution};
         }
         return result;
+    }
+
+    private boolean equalDoubles(double a, double b) {
+        return Double.compare(Math.abs(a - b), Math.pow(10, PRECISION)) < 0;
     }
 }
--- src/test/java/com/example/equation/QuadraticEquationTest.java
+++ src/test/java/com/example/equation/QuadraticEquationTest.java
@@ -2,10 +2,12 @@ package com.example.equation;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class QuadraticEquationTest {
     QuadraticEquation cut = new QuadraticEquation();
@@ -37,5 +39,33 @@ class QuadraticEquationTest {
         assertNotNull(result);
         assertEquals(2, result.length);
         assertArrayEquals(new double[]{-1, 1}, result);
+    }
+
+    @Test
+    @DisplayName("Equation x^2+2x+1 = 0 has 1 root, -1")
+    void testEquationHasOneRoot() {
+        // arrange
+        double a = 1;
+        double b = 2;
+        double c = 1;
+        // act
+        var result = cut.solve(a, b, c);
+        // assert
+        assertNotNull(result);
+        assertEquals(1, result.length);
+        assertArrayEquals(new double[]{-1}, result);
+    }
+
+    @Test
+    @DisplayName("Exception when coefficient a is equal to 0")
+    void testIfCoefAIsZeroThenException() {
+        // arrange
+        double a = 0;
+        double b = 1;
+        double c = 1;
+        // act
+        final Executable executable = () -> cut.solve(a, b, c);
+        // assert
+        assertThrows(IllegalArgumentException.class, executable);
     }
 }
