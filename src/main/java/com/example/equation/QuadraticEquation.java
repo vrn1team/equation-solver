eae4f33a7890da2a3a5393e71c87797a9ceaf2d7
Белоушкин Александр Николаевич <aleks.beloushkin@yahoo.com>
2022-12-11 19:37:28 +0300

    Add test for equation with two roots, add minimal implementation (discriminant > 0)

--- src/main/java/com/example/equation/QuadraticEquation.java
+++ src/main/java/com/example/equation/QuadraticEquation.java
@@ -5,9 +5,13 @@ public class QuadraticEquation {
 
     public double[] solve(double a, double b, double c) {
         double[] result = null;
-        double d = b * b - 4 * a * c;
-        if (d < 0) {
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) {
             result = new double[]{};
+        } else if (discriminant > 0) {
+            double solution1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            double solution2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            result = new double[]{solution1, solution2};
         }
         return result;
     }
--- src/test/java/com/example/equation/QuadraticEquationTest.java
+++ src/test/java/com/example/equation/QuadraticEquationTest.java
@@ -3,6 +3,7 @@ package com.example.equation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -21,5 +22,20 @@ class QuadraticEquationTest {
         // assert
         assertNotNull(result);
         assertEquals(0, result.length);
+    }
+
+    @Test
+    @DisplayName("Equation x^2-1 = 0 has 2 roots, 1 and -1")
+    void testEquationHasTwoRoots() {
+        // arrange
+        double a = 1;
+        double b = 0;
+        double c = -1;
+        // act
+        var result = cut.solve(a, b, c);
+        // assert
+        assertNotNull(result);
+        assertEquals(2, result.length);
+        assertArrayEquals(new double[]{-1, 1}, result);
     }
 }
